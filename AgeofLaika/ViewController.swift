71ce71bb801effc9ecb04a90c4f10f6de8777594
edmar-trimorph <edmar.posesano@trimor.ph>
2014-11-25 16:30:00 +0800

    Initial Commit

---
+++ AgeofLaika/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AgeofLaika
+//
+//  Created by Edmar P. on 11/25/14.
+//  Copyright (c) 2014 Edmar P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
